4134727e32cb5f31a373db603f2f6dd6b32045d8
MikeAchoy <miguel.andres.achoy@gmail.com>
2025-01-07 20:28:09 -0600

    Added implementation for current challenge

--- main.py
+++ main.py
@@ -184,7 +184,20 @@ Given an array of string words, return all strings in words that is a substring 
 A substring is a contiguous sequence of characters within a string
 '''
 def stringMatching(words: list[str]) -> list[str]:
-    pass
+    matching_words = []
+
+    # Iterate through each word in the input list.
+    for current_word_index in range(len(words)):
+        # Compare the current word with all other words.
+        for other_word_index in range(len(words)):
+            # Skip comparing the word with itself.
+            if current_word_index == other_word_index:
+                continue
+            if words[current_word_index] in words[other_word_index]:
+                # Add it to the result list if true.
+                matching_words.append(words[current_word_index])
+                break  # No need to check further for this word.
+    return matching_words
 
 
 def main():
