e59571a37a3904049763e175b983f527ad9742c6
Mikeab <97768019+MikeAchoy@users.noreply.github.com>
2025-01-15 13:38:03 -0600

    Added implementation for Jan. 15 challenge

--- main.py
+++ main.py
@@ -400,8 +400,32 @@ Return the integer x. The test cases are generated such that x is uniquely deter
 The number of set bits of an integer is the number of 1's in its binary representation.
 '''
 def minimizeXor(num1: int, num2: int) -> int:
-    pass
+    # Helper functions for setting and checking bits:
+    # Helper function to check if the given bit position in x is set (1)
+    def _is_set(x: int, bit: int) -> bool:
+        return (x & (1 << bit)) != 0
 
+    # Helper function to set the given bit position in x to 1
+    def _set_bit(x: int, bit: int):
+        return x | (1 << bit)
+
+    result = 0
+
+    target_set_bits_count = bin(num2).count("1")
+    set_bits_count = 0
+    current_bit = 31  # Start from the most significant bit
+
+    # While result has fewer set bits than num2
+    while set_bits_count < target_set_bits_count:
+        # If the current bit of num1 is set or we must set all remaining bits in result
+        if _is_set(num1, current_bit) or (
+            target_set_bits_count - set_bits_count > current_bit
+        ):
+            result = _set_bit(result, current_bit)
+            set_bits_count += 1
+
+        current_bit -= 1  # Move to the next bit
+    return result
 
 
 def main():
