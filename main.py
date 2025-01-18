dd7950258a1c02413c42b70f050ed8a235a998d5
Mikeab <97768019+MikeAchoy@users.noreply.github.com>
2025-01-18 11:22:20 -0600

    Added challenge and implementation for Jan. 17

--- main.py
+++ main.py
@@ -491,6 +491,40 @@ def xorAllNums(nums1: list[int], nums2: list[int]) -> int:
     return ans
 
 
+'''
+January 17, 2025
+2683. Neighboring Bitwise XORa:
+A 0-indexed array derived with length n is derived by computing the bitwise XOR (⊕) of adjacent values in a binary array original of length n.
+Specifically, for each index i in the range [0, n - 1]:
+If i = n - 1, then derived[i] = original[i] ⊕ original[0].
+Otherwise, derived[i] = original[i] ⊕ original[i + 1].
+Given an array derived, your task is to determine whether there exists a valid binary array original that could have formed derived.
+Return true if such an array exists or false otherwise.
+A binary array is an array containing only 0's and 1's
+'''
+def doesValidArrayExist(derived: list[int]) -> bool:
+    # Create an original array initialized with 0.
+    original = [0]
+    for i in range(len(derived)):
+        original.append(derived[i] ^ original[i])
+
+    # Store the validation results in checkForZero and checkForOne respectively.
+    check_for_zero = original[0] == original[-1]
+    original = [1]
+    for i in range(len(derived)):
+        original.append(derived[i] ^ original[i])
+    check_for_one = original[0] == original[-1]
+
+    return check_for_zero or check_for_one
+
+
+'''
+January 18, 2025
+
+'''
+
+
+
 def main():
     pass
 
